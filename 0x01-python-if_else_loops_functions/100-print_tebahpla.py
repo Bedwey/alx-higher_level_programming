2bdf4a225c47ec06fae0b12ffa2c01674439ac92
Bedwey <bedweyy@gmail.com>
2023-06-06 19:18:44 -0700

    15. Remove at position

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for ch in range(-122, -96):
-    print(chr(abs(ch)) if abs(ch) % 2 == 0 else chr(abs(ch) - 32), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
