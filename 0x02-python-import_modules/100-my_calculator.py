a9599a3b9e9a55eeb7c57beba280fe49a0e4412e
Bedwey <bedweyy@gmail.com>
2023-06-08 17:30:07 -0700

    fix task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, div, mul
-
 if __name__ == "__main__":
-    argvs = sys.argv
-    argc = len(argvs)
-
-    if (argc - 1) != 3:
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    a = int(argvs[1])
-    b = int(argvs[3])
-
-    if argvs[2] == '+':
-        print("{0} + {1} = {2}".format(a, b, add(a, b)))
-    elif argvs[2] == '-':
-        print("{0} - {1} = {2}".format(a, b, sub(a, b)))
-    elif argvs[2] == '/':
-        print("{0} / {1} = {2}".format(a, b, div(a, b)))
-    elif argvs[2] == '*':
-        print("{0} * {0} = {2}".format(a, b, mul(a, b)))
-    else:
+        sys.exit(1)
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(
+        a, sys.argv[2], b, operators[sys.argv[2]](a, b)
+    ))
