d11ad0ba9de41b2d25af5cd05bfa9766be026fa7
Bedwey <bedweyy@gmail.com>
2023-07-11 14:02:05 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+
+"""
+Author: Bedwey
+Date: 11/7/2023
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object as a string.
+
+    Args:
+        my_obj: The object to be serialized.
+
+    Returns:
+        A string containing the JSON representation of the object.
+    """
+    return json.dumps(my_obj)
