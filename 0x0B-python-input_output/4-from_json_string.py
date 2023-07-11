b1f5c90d78391c9f44ab370ad944b0d8f5bad842
Bedwey <bedweyy@gmail.com>
2023-07-11 14:05:15 -0700

    tsak 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import json
+
+"""
+Author: Bedwey
+Date: 11/7/2023
+"""
+
+
+def from_json_string(my_str):
+    """
+    Returns the JSON representation of an object as a string.
+
+    Args:
+        my_obj: The object to be serialized.
+
+    Returns:
+        A string containing the JSON representation of the object.
+    """
+    return json.loads(my_str)
