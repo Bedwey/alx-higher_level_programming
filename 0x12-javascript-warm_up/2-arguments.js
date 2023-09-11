8a6bcd60e1bf5fa2b44cde9a76d862a98f893a2f
Bedwey <bedweyy@gmail.com>
2023-09-11 13:30:47 -0700

    fix task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length == 3) {
-  console.log('Argument found');
-} else if (process.argv.length > 3) {
-  console.log('Arguments found');
-} else {
+const num = process.argv.length;
+if (num === 2) {
   console.log('No argument');
+} else if (num === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
