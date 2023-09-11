cb24d1f392e012b2f68018f1fc8bd4e42cc3778c
Bedwey <bedweyy@gmail.com>
2023-09-11 12:45:12 -0700

    factorial

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,4 @@ function add (a, b) {
   return (a + b);
 }
 
-if (isNaN(process.argv[2]) && isNaN(process.argv[3])) {
-  console.log('NaN');
-} else {
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
