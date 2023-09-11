d6a74cf4bf622ccdd7e5835c30e3985cba799117
Bedwey <bedweyy@gmail.com>
2023-09-11 11:56:08 -0700

    An Integer

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (process.argv.length == 3) {
   console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
