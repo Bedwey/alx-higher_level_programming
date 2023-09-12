9e0af50d46f51f3ff90518a531f2947758fc03e4
Bedwey <bedweyy@gmail.com>
2023-09-12 13:43:37 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const newList = [];
-for (let i = 0; i < list.length; i++) {
-  newList.push(list[i] * i);
-}
-console.log(newList);
+console.log(list.map((x, index) => x * index));
