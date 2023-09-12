567e48d5dd26ebcbe917445fc447e40aab0bf76e
Bedwey <bedweyy@gmail.com>
2023-09-12 13:05:24 -0700

    Square #1 use old

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        let size = '';
-        for (let i = 0; i < this.width; i++) {
-          size += c;
-        }
-        console.log(size);
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let size = '';
+      for (let i = 0; i < this.width; i++) {
+        size += c;
       }
+      console.log(size);
     }
   }
 };
