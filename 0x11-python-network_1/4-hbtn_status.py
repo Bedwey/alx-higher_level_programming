140f9e7bf21a9162a82bfd96e5103c74822882fa
Abdullah Bedwey <bedweyy@gmail.com>
2023-09-29 11:22:41 -0700

    add docs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-r = requests.get('https://alx-intranet.hbtn.io/status')
-
-print("\t- type:", type(r.text))
-print("\t- content:", r.text)
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
